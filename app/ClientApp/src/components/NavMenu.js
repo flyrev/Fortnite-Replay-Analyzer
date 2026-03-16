fcbd32b9f01c2a88968eb41a502e759e3ab7d389
Christian Neverdal <christian.neverdal@telenor.no>
2026-03-16 17:49:43 +0100

    fix: update Bootstrap 4 utility class mr-2 to Bootstrap 5 me-2

--- app/ClientApp/src/components/NavMenu.js
+++ app/ClientApp/src/components/NavMenu.js
@@ -27,7 +27,7 @@ export class NavMenu extends Component {
         <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <Container>
             <NavbarBrand tag={Link} to="/">Fortnite Replay Analyzer</NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+            <NavbarToggler onClick={this.toggleNavbar} className="me-2" />
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
